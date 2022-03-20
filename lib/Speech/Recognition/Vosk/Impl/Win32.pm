83fea8672515664a4c5f941a725bfdb040bde433
Max Maischein <corion@corion.net>
2022-03-20 20:13:13 +0100

    Move code, add SYNOPSIS

--- lib/Speech/Recognition/Vosk/Impl/Win32.pm
+++ lib/Speech/Recognition/Vosk/Impl/Win32.pm
@@ -6,6 +6,18 @@ use Win32::API;
 use Exporter 'import';
 use File::ShareDir 'dist_dir';
 
+our $VERSION = '0.03';
+our @EXPORT_OK = (qw(
+    model_new
+    model_find_word
+    model_recognizer_new
+    model_recognizer_free
+    model_recognizer_accept_waveform
+    model_recognizer_partial_result
+    model_recognizer_result
+    model_recognizer_final_result
+));
+
 # We want to load the DLLs from here:
 sub load_libvosk {
     my ($path) = @_;
@@ -21,6 +33,9 @@ sub load_libvosk {
     Win32::API::More->Import("libvosk.dll", "vosk_recognizer_new", "NF", "N")
         or die $^E;
 
+    Win32::API::More->Import("libvosk.dll", "vosk_recognizer_free", "N", "N")
+        or die $^E;
+
     Win32::API->Import("libvosk.dll", "vosk_recognizer_accept_waveform", "NPN", "N")
         or die $^E;
 
@@ -32,18 +47,61 @@ sub load_libvosk {
 
     Win32::API->Import("libvosk.dll", "vosk_recognizer_final_result", "N", "P")
         or die $^E;
-}
 
-our @EXPORT_OK = (qw(
-    model_new
-    model_find_word
-    model_recognizer_new
-    model_recognizer_accept_waveform
-    model_recognizer_partial_result
-    model_recognizer_result
-    model_recognizer_final_result
-));
+    # Make the names available without their "vosk_" prefix:
+    for my $name (@EXPORT_OK) {
+        no strict 'refs';
+        my $imported = "vosk_" . $name;
+        *{ $name } = \&{$imported};
+    }
+}
 
 load_libvosk();
 
 1;
+
+=head1 NAME
+
+Speech::Recognition::Vosk::Impl::Win32 - Win32 library for the Vosk toolkit
+
+=head1 SYNOPSIS
+
+Most likely, you want to use the more convenient OO wrapper in
+L<Speech::Recognition::Vosk::Recognizer>.
+
+  use Speech::Recognition::Vosk::Impl::Win32 qw(
+  );
+  use JSON 'decode_json';
+
+  my $model = Speech::Recognition::Vosk::Impl::Win32::model_new("model-en");
+  my $recognizer = Speech::Recognition::Vosk::Impl::Win32::recognizer_new($model, 44100);
+
+  binmode STDIN, ':raw';
+
+  while( ! eof(*STDIN)) {
+      read(STDIN, my $buf, 3200);
+      my $complete = Speech::Recognition::Vosk::Impl::Win32::recognizer_accept_waveform($recognizer, $buf);
+      my $spoken;
+      if( $complete ) {
+          $spoken = Speech::Recognition::Vosk::Impl::Win32::recognizer_result($recognizer);
+      } else {
+          $spoken = Speech::Recognition::Vosk::Impl::Win32::recognizer_partial_result($recognizer);
+      }
+
+      my $info = decode_json($spoken);
+      if( $info->{text}) {
+          print $info->{text},"\n";
+      } else {
+          local $| = 1;
+          print $info->{partial}, "\r";
+      };
+  }
+
+  # Flush the buffers
+  my $spoken = Speech::Recognition::Vosk::Impl::Win32::recognizer_final_result($recognizer);
+  my $info = decode_json($spoken);
+  print $info->{text},"\n";
+
+=head1 FUNCTIONS
+
+=cut
